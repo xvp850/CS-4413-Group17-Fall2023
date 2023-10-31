47cb7ff367446131898220e66e2a2b83c7adb9d0
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-10-31 13:40:59 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,26 +1,21 @@
 <?php
 
-// connect.php
-
+//connect.php
 $server = 'xvp850-mysql.mysql.database.azure.com';
-
 $username = 'xvp850';
-
 $password = 'Mypassword1';
-
 $database = 'cs4413-group17-database';
-
 $db_connection = mysqli_connect($server, $username, $password, $database);
 
 // Check connection
 if (mysqli_connect_errno())
 {
-    //echo 'Connection to database failed:'.mysqli_connect_error();
+    echo 'Connection to database failed:'.mysqli_connect_error();
     exit();
 }
 
-//echo "database connection success<br>";
-//echo "<strong>now showing results from a database query...</strong>";
+echo "database connection success<br>";
+echo "<strong>now showing results from a database query...</strong>";
 
 // Select database
 if (!mysqli_select_db($db_connection, $database)) {
