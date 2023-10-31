c0601768739abbd337d200bde4069aaa819dd75d
BrandonEvins <111911993+BrandonEvins@users.noreply.github.com>
2023-10-31 16:57:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
 <?php
-
-//index.php
-
+// create_cat.php 
 include 'connect.php';
-
 include 'header.php';
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 $sql = "SELECT 
 cat_id, 
 cat_name, 
 cat_description 
 FROM 
 categories";
-$result = mysqli_query($db_connection, $sql);
+$result = mysqli_query($conn, $sql);
 if (!$result) {
     echo 'The categories could not be displayed, please try again later.';
 } else {
@@ -27,28 +21,18 @@ if (!$result) {
 <th>Category</th> 
 <th>Last topic</th> 
 </tr>';
-
-		while($row = mysqli_fetch_assoc($result)){
-			echo '<tr>';
-
-			echo '<td class="leftpart">';
-
-			echo '<h3><a href="category.php?id=' . $row['cat_name'] . '</a></h3>' . $row['cat_description'];
-
-			echo '</td>';
-
-			echo '<td class="rightpart">';				
-
-			echo '<a href="topic.php?id=">Topic subject</a> at 10-10';
-
-			echo '</td>';
-
-			echo '</tr>';
-		}
-	}
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<tr>';
+            echo '<td class="leftpart">';
+            echo '<h3><a href="category.php?id">' . $row['cat_name'] . '</a></h3>' . $row['cat_description'];
+            echo '</td>';
+            echo '<td class="rightpart">';
+            echo '<a href="topic.php?id=">Topic subject</a> at 10-10';
+            echo '</td>';
+            echo '</tr>';
+        }
+    }
 }
-
 include 'footer.php';
-
 ?>
 
