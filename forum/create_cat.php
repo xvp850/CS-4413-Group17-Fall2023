36140acd010205f973c2e936204f9b3c8c0e6e92
BrandonEvins <111911993+BrandonEvins@users.noreply.github.com>
2023-10-31 13:58:32 -0500

    Update create_cat.php

--- forum/create_cat.php
+++ forum/create_cat.php
@@ -1,5 +1,7 @@
 <?php
-include 'connect.php';
+include './connect.php';
+include './header.php';
+
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     // The form hasn't been posted yet, display it 
     echo "<form method='post' action=''> 
