4385614d2a19b377c81964b604966a509b9db5b7
BrandonEvins <111911993+BrandonEvins@users.noreply.github.com>
2023-10-31 16:04:24 -0500

    change file location

--- create_cat.php
+++ create_cat.php
@@ -1,6 +1,6 @@
 <?php
-include '../connect.php';
-include '../header.php';
+include 'connect.php';
+include 'header.php';
 
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     // The form hasn't been posted yet, display it 
