06b4fc8766dc55c0a6fe43d0bf2023208b6e6f27
BrandonEvins <111911993+BrandonEvins@users.noreply.github.com>
2023-10-31 13:12:37 -0500

    Update header.php

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <h1>My forum</h1>
 	<div id="wrapper">
 	<div id="menu">
-		<a class="item" href="/forum/index.php">Home</a> -
+		<a class="item" href="index.php">Home</a> -
 		<a class="item" href="/forum/create_topic.php">Create a topic</a> -
 		<a class="item" href="/forum/create_cat.php">Create a category</a>
 		<div id="userbar">
