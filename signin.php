a2d9589ac864e0c5f63bd66f2f0f4ff3fd4bdbc7
BrandonEvins <111911993+BrandonEvins@users.noreply.github.com>
2023-11-01 18:46:06 -0500

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,113 +1,74 @@
 <?php
-//signin.php
+session_start();
 include 'connect.php';
 include 'header.php';
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 echo '<h3>Sign in</h3>';
 
-//first, check if the user is already signed in. If that is the case, there is no need to display this page
-if(isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true)
-{
+if (isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true) {
     echo 'You are already signed in, you can <a href="signout.php">sign out</a> if you want.';
-}
+} else {
+    if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+        echo '<form method="post" action="">
+        Username: <input type="text" name="user_name" />
+        Password: <input type="password" name="user_pass">
+        <input type="submit" value="Sign in" />
+        </form>';
+    } else {
+        $errors = array();
 
-else
-{
-    if($_SERVER['REQUEST_METHOD'] != 'POST')
-	{
-		/*the form hasn't been posted yet, display it
-		note that the action="" will cause the form to post to the same page it is on */
-		echo '<form method="post" action="">
-		Username: <input type="text" name="user_name" />
-		Password: <input type="password" name="user_pass">
-		<input type="submit" value="Sign in" />
-		</form>';
-	}
-	
-	else
-	{
-		/* so, the form has been posted, we'll process the data in three steps:
-		1. Check the data
-		2. Let the user refill the wrong fields (if necessary)
-		3. Varify if the data is correct and return the correct response
-		*/
-		$errors = array(); /* declare the array for later use */
-		if(!isset($_POST['user_name']))
-		{
-			$errors[] = 'The username field must not be empty.';
-		}
-		if(!isset($_POST['user_pass']))
-		{
-			$errors[] = 'The password field must not be empty.';
-		}
+        if (empty($_POST['user_name'])) {
+            $errors[] = 'The username field must not be empty.';
+        }
+        if (empty($_POST['user_pass'])) {
+            $errors[] = 'The password field must not be empty.';
+        }
 
-		if(!empty($errors)) /*check for an empty array, if there are errors, they're in this array (note the ! operator)*/
-		{
-			echo 'There are error in the fields and are not filled in correctly.';
-			echo '<ul>';
+        if (!empty($errors)) {
+            echo 'There are errors in the fields and they are not filled in correctly.';
+            echo '<ul>';
 
-			foreach($errors as $key => $value) /* walk through the array so all the errors get displayed */
-			{
-				echo '<li>' . $value . '</li>'; /* this generates a nice error list */
-			}
-			echo '</ul>';
-		}
-		
-		else
-		{
-			//the form has been posted without errors, so save it
-			//notice the use of mysql_real_escape_string, keep everything safe!
-			//also notice the sha1 function which hashes the password
-			$sql = "SELECT user_id, user_name, user_level FROM users WHERE user_name = ? AND user_pass = ?";
-            		$stmt = mysqli_prepare($db_connection, $sql);
-            		if ($stmt) {
-						// Bind parameters using variables
-						$username = $_POST['user_name'];
-						$password = sha1($_POST['user_pass']); // Hash the password
-					
-						mysqli_stmt_bind_param($stmt, 'ss', $username, $password);
-					}
-            		$result = mysqli_stmt_execute($stmt);
+            foreach ($errors as $key => $value) {
+                echo '<li>' . $value . '</li>';
+            }
+            echo '</ul>';
+        } else {
+            $sql = "SELECT user_id, user_name, user_level FROM users WHERE user_name = ? AND user_pass = ?";
+            $stmt = mysqli_prepare($db_connection, $sql);
 
-			if(!$result)
-			{
-				//something went wrong, display the error
-				echo 'Something went wrong while signing in. Please try again later.';
-				//echo mysql_error(); //debugging purposes, uncomment when needed
-				//echo mysqli_error($db_connection); // Display the mysqli error
-			}
-			else
-			{
-			//the query was successfully executed, there are 2 possibilities
-			//1. the query returned data, the user can be signed in
-			//2. the query returned an empty result set, the credentials were wrong
-                	$result = mysqli_stmt_get_result($stmt);
+            if ($stmt) {
+                $username = $_POST['user_name'];
+                $password = sha1($_POST['user_pass']); // Hash the password
 
-				if (mysqli_num_rows($result) == 0)
-				{
-					echo 'You have supplied a wrong user/password combination. Please try again.';
-				}
-				else
-				{
-					//set the $_SESSION['signed_in'] variable to TRUE
-					$_SESSION['signed_in'] = true;
+                mysqli_stmt_bind_param($stmt, 'ss', $username, $password);
+                mysqli_stmt_execute($stmt);
+                $result = mysqli_stmt_get_result($stmt);
 
-					//we also put the user_id and user_name values in the $_SESSION, so we can use it at various pages
-					while ($row = mysqli_fetch_assoc($result))
-					{
-						$_SESSION['user_id'] 	= $row['user_id'];
-						$_SESSION['user_name'] 	= $row['user_name'];
-						$_SESSION['user_level'] = $row['user_level'];
-					}
-					echo 'Welcome, ' . $_SESSION['user_name'] . '. <a href="index.php">Proceed to the forum overview</a>.';
-				}
-			}
-		}
-	}
+                if (!$result) {
+                    echo 'Something went wrong while signing in. Please try again later.';
+                } else {
+                    if (mysqli_num_rows($result) == 0) {
+                        echo 'You have supplied a wrong user/password combination. Please try again.';
+                    } else {
+                        $row = mysqli_fetch_assoc($result);
+
+                        $_SESSION['signed_in'] = true;
+                        $_SESSION['user_id'] = $row['user_id'];
+                        $_SESSION['user_name'] = $row['user_name'];
+                        $_SESSION['user_level'] = $row['user_level'];
+
+                        echo 'Welcome, ' . $_SESSION['user_name'] . '. <a href="index.php">Proceed to the forum overview</a>.';
+                    }
+                }
+            } else {
+                echo "Error in preparing the statement: " . mysqli_error($db_connection);
+            }
+        }
+    }
 }
 
 include 'footer.php';
 ?>
-
