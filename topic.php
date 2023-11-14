e9144b1c86ab833816933176911f96b9a642d7e1
BrandonEvins <111911993+BrandonEvins@users.noreply.github.com>
2023-11-14 12:20:08 -0600

    Update topic.php

--- topic.php
+++ topic.php
@@ -8,48 +8,56 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 if (isset($_GET['id']) && is_numeric($_GET['id'])) {
-    $id = $_GET['id'];
+    $topic_id = $_GET['id'];
 
-    $sql = "SELECT 
-                topic_id, 
-                topic_subject, 
-                topic_date 
-            FROM 
-                topics 
-            WHERE 
-                topic_cat = $id"; // Ensure $id is a numeric value
+    // Retrieve the topic information
+    $topic_sql = "SELECT 
+                    topic_subject, 
+                    topic_date 
+                FROM 
+                    topics 
+                WHERE 
+                    topic_id = $topic_id";
 
-    $result = mysqli_query($db_connection, $sql);
+    $topic_result = mysqli_query($db_connection, $topic_sql);
 
-    if (!$result) {
-        echo 'The topics could not be displayed, please try again later.';
-    } else {
-        if (mysqli_num_rows($result) === 0) {
-            echo 'There are no topics in this category yet.';
-        } else {
-            echo '<h2>Topics in this category</h2>';
-            echo '<table border="1"> 
-                    <tr> 
-                        <th>Topic</th> 
-                        <th>Created at</th> 
-                    </tr>';
+    if ($topic_result && mysqli_num_rows($topic_result) > 0) {
+        $topic_row = mysqli_fetch_assoc($topic_result);
 
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo '<tr>';
-                echo '<td class="leftpart">';
-                echo '<h3><a href="topic_content.php?id=' . $row['topic_id'] . '">' . $row['topic_subject'] . '</a></h3>';
-                echo '</td>';
-                echo '<td class="rightpart">';
-                echo date('d-m-Y', strtotime($row['topic_date']));
-                echo '</td>';
-                echo '</tr>';
+        echo '<h2>' . $topic_row['topic_subject'] . '</h2>';
+        echo '<p>Created at: ' . date('d-m-Y', strtotime($topic_row['topic_date'])) . '</p>';
+
+        // Retrieve and display posts for the selected topic
+        $posts_sql = "SELECT 
+                        post_content, 
+                        post_date 
+                    FROM 
+                        posts 
+                    WHERE 
+                        post_topic = $topic_id";
+
+        $posts_result = mysqli_query($db_connection, $posts_sql);
+
+        if ($posts_result && mysqli_num_rows($posts_result) > 0) {
+            echo '<h3>Posts</h3>';
+            echo '<ul>';
+
+            while ($post_row = mysqli_fetch_assoc($posts_result)) {
+                echo '<li>';
+                echo '<p>' . $post_row['post_content'] . '</p>';
+                echo '<p>Posted at: ' . date('d-m-Y', strtotime($post_row['post_date'])) . '</p>';
+                echo '</li>';
             }
 
-            echo '</table>';
+            echo '</ul>';
+        } else {
+            echo 'No posts for this topic yet.';
         }
+    } else {
+        echo 'Topic not found.';
     }
 } else {
-    echo "Category ID is not set or invalid.";
+    echo "Topic ID is not set or invalid.";
 }
 
 include 'footer.php';
