a7db4aa76c0b60d913ab734e273c4c54537b7ca9
BrandonEvins <111911993+BrandonEvins@users.noreply.github.com>
2023-11-14 12:54:58 -0600

    Update reply.php

--- reply.php
+++ reply.php
@@ -4,28 +4,31 @@ include 'connect.php';
 include 'header.php';
 
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    // someone is calling the file directly, which we don't want
     echo 'This file cannot be called directly.';
 } else {
+    // check for sign-in status
     if (!$_SESSION['signed_in']) {
         echo 'You must be signed in to post a reply.';
     } else {
-        $sql = "INSERT INTO 
-                    posts(post_content, 
-                    post_date, 
-                    post_topic, 
-                    post_by) 
+        // a real user posted a real reply
+        $sql = "INSERT INTO posts (post_content, post_date, post_topic, post_by) 
                 VALUES (?, NOW(), ?, ?)";
-        
-        $stmt = mysqli_prepare($conn, $sql);
-        mysqli_stmt_bind_param($stmt, "sii", $_POST['reply-content'], $_GET['id'], $_SESSION['user_id']);
-        mysqli_stmt_execute($stmt);
+        $stmt = mysqli_prepare($db_connection, $sql);
 
-        if (mysqli_stmt_errno($stmt)) {
-            echo 'Your reply has not been saved, please try again later.';
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, "sii", $_POST['reply-content'], $_GET['id'], $_SESSION['user_id']);
+            mysqli_stmt_execute($stmt);
+
+            if (mysqli_stmt_errno($stmt)) {
+                echo 'Your reply has not been saved, please try again later.';
+            } else {
+                echo 'Your reply has been saved, check out <a href="topic.php?id=' . htmlentities($_GET['id']) . '">the topic</a>.';
+            }
+
+            mysqli_stmt_close($stmt);
         } else {
-            // Redirect back to the topic page after posting the reply
-            header("Location: topic.php?id=" . htmlentities($_GET['id']));
-            exit(); // Ensure script stops executing after the redirect
+            echo 'Error preparing statement: ' . mysqli_error($db_connection);
         }
     }
 }
