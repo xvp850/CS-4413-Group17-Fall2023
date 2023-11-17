2733783bb155649de258d29de7b1ff8148ddafaf
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-17 15:45:16 -0600

    Update header.php
    changed create_cat to create_category

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 	<div id="menu">
 		<a class="item" href="index.php">Home</a> -
 		<a class="item" href="create_topic.php">Create a topic</a> -
-		<a class="item" href="create_cat.php">Create a category</a>
+		<a class="item" href="create_category.php">Create a category</a>
 		<div id="userbar">
         <div id="userbar">
 			<?php
