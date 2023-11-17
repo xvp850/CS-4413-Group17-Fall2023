6c23710dee4b5f25ef8f946efde067bf4c88b1a8
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-17 15:47:56 -0600

    Update create_category.php
    Clean clean clean

--- create_category.php
+++ create_category.php
@@ -2,28 +2,36 @@
 /* This is create_category.php */
 
 session_start();
+
 include 'connect.php';
 include 'header.php';
+
 error_reporting(E_ALL);
+
 ini_set('display_errors', 1);
+
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     // The form hasn't been posted yet, display it 
     echo "<form method='post' action=''> 
-Category name: <input type='text' name='cat_name' /> 
-Category description: <textarea name='cat_description'></textarea> 
+Category name: <input type='text' name='category_name' /> 
+Category description: <textarea name='category_description'></textarea> 
 <input type='submit' value='Add category' /> 
 </form>";
 } else {
     // The form has been posted, so save it 
-    $cat_name = $_POST['cat_name'];
-    $cat_description = $_POST['cat_description'];
+    $category_name = $_POST['category_name'];
+    $category_description = $_POST['category_description'];
+    
     // Prepare the SQL statement with placeholders 
-    $sql = "INSERT INTO categories (cat_name, cat_description) 
+    $sql = "INSERT INTO categories (category_name, category_description) 
 VALUES (?, ?)";
+    
     // Create a prepared statement 
     $stmt = mysqli_prepare($db_connection, $sql);
+    
     // Bind parameters to the prepared statement 
-    mysqli_stmt_bind_param($stmt, 'ss', $cat_name, $cat_description);
+    mysqli_stmt_bind_param($stmt, 'ss', $category_name, $category_description);
+    
     // Execute the prepared statement 
     $result = mysqli_stmt_execute($stmt);
     if (!$result) {
