835fdcb971531027e732bd922b84cc93ebd2ff75
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-26 12:01:42 -0600

    Create sign_out.php
    muhahaha

---
+++ sign_out.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include 'connect.php';
+include 'header.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo '<h3>Sign Out</h3>';
+
+if (isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true) {
+    session_unset();
+    session_destroy();
+    echo 'Goodbye onii-chan. You are signed out, you can <a href="sign_in.php">sign in</a> again if you want.';
+} else {
+    echo 'You are already signed out, you can <a href="sign_out.php">sign out</a> if you want.';
+}
+
+include 'footer.php';
+?>
