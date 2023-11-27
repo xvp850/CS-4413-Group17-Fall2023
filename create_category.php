2124de0abe4cbaef599cac9eac30a8ee2925e3f2
BrandonEvins <111911993+BrandonEvins@users.noreply.github.com>
2023-11-27 12:07:51 -0600

    Update create_category.php

--- create_category.php
+++ create_category.php
@@ -1,7 +1,7 @@
 <?php
 /* This is create_category.php */
 
-//Maybe make this admin only?
+// Maybe make this admin only?
 
 session_start();
 
@@ -9,40 +9,50 @@ include 'connect.php';
 include 'header.php';
 
 error_reporting(E_ALL);
-
 ini_set('display_errors', 1);
 
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-    // The form hasn't been posted yet, display it 
-    echo "<form method='post' action=''> 
-        Category name: <input type='text' name='category_name' />
-        <br><br>Category description: <br><textarea name='category_description'></textarea> 
-        <input type='submit' value='Add category' /> 
-        </form>";
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    echo "Must be logged in to create a category. Please sign in.";
 } else {
-    // The form has been posted, so save it 
-    $cat_name = $_POST['category_name'];
-    $cat_description = $_POST['category_description'];
-    
-    // Prepare the SQL statement with placeholders 
-    $sql = "INSERT INTO categories (cat_name, cat_description) 
-            VALUES (?, ?)";
-    
-    // Create a prepared statement 
-    $stmt = mysqli_prepare($db_connection, $sql);
-    
-    // Bind parameters to the prepared statement 
-    mysqli_stmt_bind_param($stmt, 'ss', $cat_name, $cat_description);
-    
-    // Execute the prepared statement 
-    $result = mysqli_stmt_execute($stmt);
-    if (!$result) {
-        // Something went wrong, display the error 
-        echo 'Error: ' . mysqli_error($db_connection);
+    // Check if the user is an admin (user_level = 0)
+    if ($_SESSION['user_level'] == 0) {
+        if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+            // The form hasn't been posted yet, display it 
+            echo "<form method='post' action=''> 
+                Category name: <input type='text' name='category_name' />
+                <br><br>Category description: <br><textarea name='category_description'></textarea> 
+                <input type='submit' value='Add category' /> 
+                </form>";
+        } else {
+            // The form has been posted, so save it 
+            $cat_name = $_POST['category_name'];
+            $cat_description = $_POST['category_description'];
+            
+            // Prepare the SQL statement with placeholders 
+            $sql = "INSERT INTO categories (cat_name, cat_description) 
+                    VALUES (?, ?)";
+            
+            // Create a prepared statement 
+            $stmt = mysqli_prepare($db_connection, $sql);
+            
+            // Bind parameters to the prepared statement 
+            mysqli_stmt_bind_param($stmt, 'ss', $cat_name, $cat_description);
+            
+            // Execute the prepared statement 
+            $result = mysqli_stmt_execute($stmt);
+            if (!$result) {
+                // Something went wrong, display the error 
+                echo 'Error: ' . mysqli_error($db_connection);
+            } else {
+                echo 'New category successfully added.';
+            }
+            
+            // Close the prepared statement 
+            mysqli_stmt_close($stmt);
+        }
     } else {
-        echo 'New category successfully added.';
+        echo "Must be admin to create a category. Please sign in as an admin.";
     }
-    // Close the prepared statement 
-    mysqli_stmt_close($stmt);
 }
 ?>
