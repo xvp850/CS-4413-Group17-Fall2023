c56970d7a6b2b375b56de3620882cf86511f17a7
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-26 23:44:10 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 <div id="footer">Do we need a footer?</div>
 <video width="320" height="240" autoplay muted>
   <source src="https://streamable.com/lkq99w" type="video/mp4">
-</video> 
+
 </body>
 
 </html>
