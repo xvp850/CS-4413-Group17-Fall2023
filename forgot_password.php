20399288f89c564b0736925fd60184cb5df1ca1e
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-26 19:23:11 -0600

    Create forgot_password.php
    Let's hope it works

---
+++ forgot_password.php
@@ -0,0 +1,68 @@
+<?php
+session_start();
+include 'connect.php';
+include 'header.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo '<h3>Sign in</h3>';
+
+if (isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true) {
+    echo 'You are already signed in, you can change your password in the <a href="user_profile.php">user profile</a> if you want.';
+} else {
+    if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+        echo '<form method="post" action="">
+        Username: <input type="text" name="user_name" /><br>
+        New Password: <input type="password" name="user_pass">
+        Password again: <input type="password" name="user_pass_check"><br>
+        <input type="submit" value="New password" />
+        </form>';
+    } else {
+        $errors = array();
+      
+        if (empty($_POST['user_pass'])) {
+            $errors[] = 'The password field must not be empty.';
+        }
+		    if($_POST['new_user_pass'] != $_POST['user_pass_check'])
+		    {
+			      $errors[] = 'The two passwords did not match.';
+		    }
+        if (!empty($errors)) {
+            echo 'There are errors in the fields and they are not filled in correctly.';
+            echo '<ul>';
+
+            foreach ($errors as $key => $value) {
+                echo '<li>' . $value . '</li>';
+            }
+            echo '</ul>';
+        } else {
+            $sql = "SELECT user_id, user_name, user_level FROM users WHERE user_name = ?";
+            $stmt = mysqli_prepare($db_connection, $sql);
+
+            if ($stmt) {
+                $username = $_POST['user_name'];
+                $password = sha1($_POST['user_pass']); // Hash the password
+
+                mysqli_stmt_bind_param($stmt, 'ss', $username, $password);
+                mysqli_stmt_execute($stmt);
+                $result = mysqli_stmt_get_result($stmt);
+
+                if (!$result) {
+                    echo 'There was an error in updating the password. Please contact customer support.';
+                } else {
+                    if (mysqli_num_rows($result) == 0) {
+                        echo 'Please try again.';
+                    } else {
+                        echo 'Your password has been reset. Did you want to <a href="sign_in.php">sign in</a>.';
+                    }
+                }
+            } else {
+                echo "Error in preparing the statement: " . mysqli_error($db_connection);
+            }
+        }
+    }
+}
+
+include 'footer.php';
+?>
