3f2c7f88ee8e6860711f52aa7a07575aff820829
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-26 23:48:21 -0600

    Update footer.php
    I stare in the depths and I see my own reflection. Who am I?

--- footer.php
+++ footer.php
@@ -3,8 +3,7 @@
 </div><!-- wrapper -->
 
 <div id="footer">Do we need a footer?</div>
-<video width="320" height="240" autoplay muted>
-  <source src="https://streamable.com/lkq99w" type="video/mp4"/>
+<div style="width:100%;height:0px;position:relative;padding-bottom:100.000%;"><iframe src="https://streamable.com/e/lkq99w" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
 
 </body>
 
