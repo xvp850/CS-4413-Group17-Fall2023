8c7b1c5c6995bf696902f2f5d964db51c3705c9c
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-26 23:43:27 -0600

    Update footer.php
    urghdf

--- footer.php
+++ footer.php
@@ -1,8 +1,6 @@
 </div><!-- content -->
  <video width="320" height="240" autoplay muted>
   <source src="https://streamable.com/lkq99w" type="video/mp4">
-  <source src="https://streamable.com/lkq99w.ogg" type="video/ogg">
-Your browser does not support the video tag.
 </video> 
 </div><!-- wrapper -->
 
