33fb9e196bebe350728d54deb80c6d69988f8e91
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-27 10:06:14 -0600

    Update user_profile.php

--- user_profile.php
+++ user_profile.php
@@ -1,11 +1,22 @@
 <?php
 session_start();
-include 'connect.php';
+//include 'connect.php';
 include 'header.php';
 
 			
 if (isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true) {
-    echo'.$_SESSION['user_name'].';
+    echo '<tr> 
+    <th>Category</th> 
+    <th>Last topic</th> 
+    </tr>';
+    
+    echo '<tr>';
+            echo '<td class="leftpart">';
+            echo '<h3> . $_SESSION['user_name'] .';
+            echo '</td>';
+            echo '<td class="rightpart">';
+            echo '</td>';
+    echo '</tr>';
 }
 	    
 include 'footer.php';
