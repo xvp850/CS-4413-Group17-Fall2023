a4822877b20aaf84c1c9f27fdf385a53090a14c9
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-26 23:42:16 -0600

    Update footer.php
    Let's get weird

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
 </div><!-- content -->
-
+ <video width="320" height="240" autoplay muted>
+  <source src="https://streamable.com/lkq99w" type="video/mp4">
+  <source src="https://streamable.com/lkq99w.ogg" type="video/ogg">
+Your browser does not support the video tag.
+</video> 
 </div><!-- wrapper -->
 
 <div id="footer">Do we need a footer?</div>
