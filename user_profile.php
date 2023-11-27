faea491ba6991c67cdfdcf8f84d32d83c57b09b4
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-26 23:17:29 -0600

    Update user_profile.php

--- user_profile.php
+++ user_profile.php
@@ -6,8 +6,8 @@ include 'header.php';
 			
 if (isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true) {
 
-    echo'$_SESSION['user_id']';
-    echo'$_SESSION['user_name']';
+    echo'.$_SESSION['user_name'].';
+}
 	    
 include 'footer.php';
 ?>
