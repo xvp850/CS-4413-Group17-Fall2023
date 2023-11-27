612859a6f4b8f885957acaf6e3c03a85d56d27fb
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-26 19:31:41 -0600

    Update forgot_password.php
    Who knows

--- forgot_password.php
+++ forgot_password.php
@@ -20,14 +20,33 @@ if (isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true) {
         </form>';
     } else {
         $errors = array();
-      
-        if (empty($_POST['user_pass'])) {
-            $errors[] = 'The password field must not be empty.';
-        }
-		    if($_POST['new_user_pass'] != $_POST['user_pass_check'])
-		    {
-			      $errors[] = 'The two passwords did not match.';
-		    }
+        if(isset($_POST['user_name']))
+	{
+		// the user name exists
+		if(!ctype_alnum($_POST['user_name']))
+		{
+			$errors[] = 'The username can only contain letters and digits.';
+		}
+		// the user name is longer than 25 characters
+		if(strlen($_POST['user_name']) > 25)
+		{
+			$errors[] = 'The username cannot be longer than 25 characters.';
+		}
+
+	} else {
+		$errors[] = 'The username field must not be empty.';
+	}
+
+	if(isset($_POST['user_pass']))
+	{
+		if($_POST['user_pass'] != $_POST['user_pass_check'])
+		{
+			$errors[] = 'The two passwords did not match.';
+		}
+	} else {
+		$errors[] = 'The password field cannot be empty.';
+	}
+	    
         if (!empty($errors)) {
             echo 'There are errors in the fields and they are not filled in correctly.';
             echo '<ul>';
@@ -37,6 +56,7 @@ if (isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true) {
             }
             echo '</ul>';
         } else {
+		
             $sql = "SELECT user_id, user_name, user_level FROM users WHERE user_name = ?";
             $stmt = mysqli_prepare($db_connection, $sql);
 
