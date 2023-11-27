67b45333c945fcd535f90be5647aaeabfe877525
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-27 09:54:11 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,6 @@
 </div><!-- wrapper -->
 
 <div id="footer">Do we need a footer?</div>
-<div style="width:100%;height:0px;position:relative;padding-bottom:100.000%;"><iframe src="https://streamable.com/e/lkq99w" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
 
 </body>
 
