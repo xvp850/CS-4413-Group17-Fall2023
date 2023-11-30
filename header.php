05215cbe43303f9c6517d0d39d85434d59d37a11
phosu7 <99696023+phosu7@users.noreply.github.com>
2023-11-30 16:36:48 -0600

    Update header.php

--- header.php
+++ header.php
@@ -46,3 +46,10 @@
         </div>
 	</div>
 		<div id="content">
+		
+		<footer>
+			<img id="footerimg" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fleosigh.com%2Fwp-content%2Fuploads%2F2022%2F12%2FBocchi-the-Rocks-Nijika-Ijichi.jpg&f=1&nofb=1&ipt=a9e2c1be80a569249480dd710e8f14341a9e55af26c3115dbc852dda61ccf731&ipo=images">
+		
+		</footer>
+
+	</body>
