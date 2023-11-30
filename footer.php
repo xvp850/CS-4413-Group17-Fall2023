831c85893a55fa4b0a855209fed0cf0262c0ba58
xvp850 <90293618+xvp850@users.noreply.github.com>
2023-11-30 15:33:45 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 </div><!-- wrapper -->
 
-<div id="footer"></div>
+<div id="footer"><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fleosigh.com%2Fwp-content%2Fuploads%2F2022%2F12%2FBocchi-the-Rocks-Nijika-Ijichi.jpg&f=1&nofb=1&ipt=a9e2c1be80a569249480dd710e8f14341a9e55af26c3115dbc852dda61ccf731&ipo=images"></div>
 
 </body>
 
